9307a8aba4b66fb4a752eeb2a42ef169854cbfac
Сергей (DonStacky) <rodnihsergey1994@mail.ru>
2023-01-24 14:50:59 +0300

    feat: finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrStr = str.split('');
+for (let i = 0; i <= bracketsConfig.length; i++) {
+for (arr of bracketsConfig) {
+  let starr = arr.join('');
+  let length = str.length;
+for (let i = 0; i < length; i++) {
+  let index = str.indexOf(starr);
+  if (index !== -1) {
+    arrStr.splice(index, 2);
+    str = arrStr.join('');
+  } else {break;};
+}}}
+return (str === '');
 }
